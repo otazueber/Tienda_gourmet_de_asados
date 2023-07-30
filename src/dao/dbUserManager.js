8233d00fde4cabef4520c91a4fcf71771d63a854
eber <otazueber@gmail.com>
2023-07-29 21:37:46 -0300

    Agrego funcionalidad (createUser y setLastConnection) al manager de usuarios

--- src/dao/dbUserManager.js
+++ src/dao/dbUserManager.js
@@ -2,10 +2,12 @@ const Users = require('../dao/models/users.model');
 const { hashPassword } = require('../utils/cryptPassword');
 
 class DbUserManager {
-
+    static async createUser(newUserInfo)
+    {
+        return await Users.create(newUserInfo);
+    }
     static async getUser(email) {
         return await Users.findOne({ email });
-
     }
     static async getUserById(id) {
         return await Users.findById({ _id: id });
@@ -17,6 +19,17 @@ class DbUserManager {
                 { email: email },
                 { password: pass },
                 { new: true }
+            );
+            return usuarioActualizado;
+        } catch (error) {
+            throw error;
+        }
+    }
+    static async setLastConnection(email) {
+        try {
+            const usuarioActualizado = await Users.findOneAndUpdate(
+                { email: email },
+                { last_connection: new Date() }
             );
             return usuarioActualizado;
         } catch (error) {
