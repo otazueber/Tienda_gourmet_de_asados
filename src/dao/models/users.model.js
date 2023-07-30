6b63bbbb2c8eb39769ccb66603b05467546140e4
eber <otazueber@gmail.com>
2023-07-29 21:36:37 -0300

    Actualizo el modelo de usuarios

--- src/dao/models/users.model.js
+++ src/dao/models/users.model.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const collectionName = 'user';
 
+const documentSchema = new mongoose.Schema({
+  name: String,
+  reference: String,
+});
+
 const collectionSchema = new mongoose.Schema({
   first_name: String,
   last_name: String,
@@ -14,11 +19,16 @@ const collectionSchema = new mongoose.Schema({
   cart: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'cart',
-},
+  },
   role: {
     type : String,
     default: 'user',
-  }
+  },
+  documents: [documentSchema],
+  last_connection: {
+    type: Date,
+    default: null,
+  },
 });
 
 const Users = mongoose.model(collectionName, collectionSchema);
